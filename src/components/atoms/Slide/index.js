64b32ba0006437d3ff65b5916c4334437553200a
Kirsten Stemmet <kirsten@bnry.local>
2019-08-26 15:12:46 +0200

    project setup commimt

--- src/components/atoms/Slide/index.js
+++ src/components/atoms/Slide/index.js
@@ -1,21 +1,22 @@
 import React, {Component} from 'react'
+import './Slide.scss'
 
 class Slide extends Component {
 	render() {
 		const { item } = this.props
 		return (
 				<li>
-					<div className="slide__image-wrapper">
+					<div className="slide-image-wrapper">
 						<img
-								className="slide__image"
+								className="slide-image"
 								alt={item.headline}
 								src={item.src}
 						/>
 					</div>
 
-					<article className="slide__content">
-						<h2 className="slide__headline">{item.headline}</h2>
-						<button className="slide__action btn">{item.button}</button>
+					<article className="slide-content">
+						<h2 className="slide-headline">{item.headline}</h2>
+						<button className="slide-action btn">{item.button}</button>
 					</article>
 				</li>
 		)
